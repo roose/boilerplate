d315fffea50d6eca1d167ef5059f11b5e66c53c8
roose <roose@mail.ru>
2014-12-21 06:20:00 +0600

    v0.3.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,91 @@
-var gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    imagemin = require('gulp-imagemin'),
-    minifycss = require('gulp-minify-css'),
-    cache = require('gulp-cache'),
-    notify = require('gulp-notify'),
-    del = require('del'),
-    livereload = require('gulp-livereload');
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var imagemin = require('gulp-imagemin');
+var minifycss = require('gulp-minify-css');
+var notify = require('gulp-notify');
+var clean = require('gulp-clean');
+var livereload = require('gulp-livereload');
+var zip = require('gulp-zip');
+// var notifier = require('node-notifier');
 
 // CSS concat to style.css and minify to style.min.css
 gulp.task('css', function() {
-  return gulp.src('src/css/*.css')
+  return gulp.src(['src/css/reset.css', 'src/css/*.css', 'src/css/main.css', '!src/css/style.css', '!src/css/style.min.css'])
     .pipe(concat('style.css'))
-    .pipe(gulp.dest('dist/css'))
+    .pipe(gulp.dest('src/css'))
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss({noAdvanced: true}))
-    .pipe(gulp.dest('dist/css'))
-    .pipe(notify({ message: 'Styles task complete' }));
+    .pipe(gulp.dest('src/css'));
 });
 
 // Fonts
 
-// JS
+// JS minify to default.min.js
 gulp.task('js', function() {
-  return gulp.src('src/js/**/*.js')
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('dist/js'))
+  return gulp.src('src/js/default.js')
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js/'))
-    .pipe(notify({ message: 'Scripts task complete' }));
+    .pipe(gulp.dest('src/js'));
 });
 
-// HTML
-gulp.task('html', function(){
+// Copy CSS
+gulp.task('copy:css', function() {
+  return gulp.src(['src/css/style.css', 'src/css/style.min.css'])
+    .pipe(gulp.dest('dist/css'));
+});
+
+// Copy JS
+gulp.task('copy:js', function() {
+  return gulp.src('src/js/**/*.js')
+    .pipe(gulp.dest('dist/js'));
+});
+
+// Copy HTML
+gulp.task('copy:html', function(){
   return gulp.src('src/*.html')
-    .pipe(gulp.dest('dist'))
-    .pipe(notify({ message: 'HTML task complete' }));
+    .pipe(gulp.dest('dist'));
 });
 
-// Images
-gulp.task('images', function() {
+// Copy Images
+gulp.task('copy:images', function() {
   return gulp.src('src/img/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('dist/img'))
-    .pipe(notify({ message: 'Images task complete' }));
+    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe(gulp.dest('dist/img'));
 });
 
 // Clean
-gulp.task('clean', function(cb) {
-  del(['dist/js', 'dist/css', 'dist/img', 'dist/*.html'], cb)
+gulp.task('clean', function() {
+  return gulp.src('dist', {read: false})
+    .pipe(clean());
+});
+
+// Build
+gulp.task('build', ['clean'], function(){
+  gulp.start(['copy:html', 'copy:css', 'copy:images', 'copy:js']);
+  // notifier.notify({title: 'Buid...', message: '...done!'}, function (err, response) {
+  //   console.log('Buid done!');
+  // });
+});
+
+// Zip dist folder
+gulp.task('zip', function () {
+    return gulp.src('dist/**/*')
+        .pipe(zip('archive.zip'))
+        .pipe(gulp.dest('.'));
 });
 
 // Watch
 gulp.task('watch', function() {
 
   gulp.watch('src/css/*.css', ['css']);
-
   gulp.watch('src/js/**/*.js', ['js']);
-
-  gulp.watch('src/*.html', ['html']);
-
-  gulp.watch('src/img/*', ['images']);
 
   // Create LiveReload server
   livereload.listen();
 
   // Watch any files in dist/, reload on change
-  gulp.watch(['dist/**']).on('change', livereload.changed);
+  gulp.watch(['src/*.html', 'src/js/**/*.js', 'src/css/*.css']).on('change', livereload.changed);
 
-});
-
-// Default
-gulp.task('default', ['clean'], function() {
-  gulp.start('css', 'js', 'html', 'images');
 });
