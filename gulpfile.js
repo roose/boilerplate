eb82015501d5011c2cd66d41c16a3f5eb105f9d3
roose <roose@mail.ru>
2017-06-15 13:08:09 +0600

    v0.8.0

--- gulpfile.js
+++ gulpfile.js
@@ -11,21 +11,22 @@ var pkg = require('./package.json')
 var paths = {
   css:   './src/css/main.css',
   img:   './src/img/**/*.{jpg,png,svg,gif}',
+  icons: './src/icons/**/*.svg',
   js:    './src/js/main.js',
   fonts: './src/fonts/**/*'
 };
 
 // errors handling and notify
-var shitHappens = function(err) {
-  $.notify.onError("Error: <%= error.message %>")(err);
+const shitHappens = function(err) {
+  $.notify.onError('Error: <%= error.message %>')(err);
   this.emit('end');
 };
 
 // autoprefix, precss css files and sourcemap
-gulp.task('css', function() {
+gulp.task('css', () => {
   var processors = [
-      autoprefixer({ browsers: ['> 1%', 'ie >= 9'] }),
-      precss
+      precss,
+      autoprefixer({ browsers: ['> 1%', 'ie >= 9'] })
   ];
   return gulp.src(paths.css)
     .pipe($.plumber({ errorHandler:shitHappens }))
@@ -36,10 +37,10 @@ gulp.task('css', function() {
 });
 
 // autoprefix, precss css files and minify
-gulp.task('build:css', function() {
+gulp.task('build:css', () => {
   var processors = [
-      autoprefixer({ browsers: ['> 1%', 'ie >= 9'] }),
       precss,
+      autoprefixer({ browsers: ['> 1%', 'ie >= 9'] }),
       csswring
   ];
   return gulp.src(paths.css)
@@ -48,74 +49,127 @@ gulp.task('build:css', function() {
     .pipe(gulp.dest('./dist/css/'));
 });
 
+gulp.task('libs', () => {
+  return gulp.src($.mainNpmFiles())
+    .pipe($.uglify())
+    .pipe($.rename({suffix: '.min'}))
+    .pipe(gulp.dest('./dist/js/'));
+});
+
 // minify main.js
-gulp.task('js', function() {
+gulp.task('js', () => {
   return gulp.src(paths.js)
     .pipe($.plumber({errorHandler:shitHappens}))
+    .pipe(gulp.dest('./dist/js/'))
     .pipe($.uglify())
+    .pipe($.rename({suffix: '.min'}))
     .pipe(gulp.dest('./dist/js/'));
 });
 
 // Copy and optimize images
-gulp.task('img', function() {
+gulp.task('img', () => {
   return gulp.src(paths.img)
     .pipe($.changed('./dist/img/'))
-    .pipe($.imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+    .pipe($.imagemin([
+      $.imagemin.svgo({
+        plugins: [
+          { removeHiddenElems: false },
+          { removeUselessDefs: false },
+          { cleanupIDs: false }
+        ]
+      }),
+      $.imagemin.gifsicle({ interlaced: true }),
+      $.imagemin.jpegtran({progressive: true }),
+      $.imagemin.optipng({ optimizationLevel: 3 })
+    ]))
+    .pipe(gulp.dest('./dist/img/'));
+});
+
+gulp.task('icons', () => {
+  return gulp.src(paths.icons)
+    .pipe($.plumber({ errorHandler:shitHappens }))
+    .pipe($.svgSymbols({
+      title: false,
+      id: 'icon_%f',
+      className: '.%f',
+      templates: [
+        'default-svg'
+      ]
+    }))
+    .pipe($.imagemin([
+      $.imagemin.svgo({
+        plugins: [
+          { removeHiddenElems: false },
+          { removeUselessDefs: false },
+          { cleanupIDs: false }
+        ]
+      })
+    ]))
+    .pipe($.rename('icons.svg'))
     .pipe(gulp.dest('./dist/img/'));
 });
 
 // Copy fonts
-gulp.task('fonts', function() {
+gulp.task('fonts', () => {
   return gulp.src(paths.fonts)
+    .pipe($.changed('./dist/fonts/'))
     .pipe(gulp.dest('./dist/fonts/'));
 });
 
 // Copy html
-gulp.task('html', function() {
+gulp.task('html', () => {
   return gulp.src('./src/*.html')
     .pipe($.changed('./dist/'))
     .pipe(gulp.dest('./dist/'));
 });
 
 // Clean, delete dist folder
-gulp.task('clean', function() {
+gulp.task('clean', () => {
   return del('./dist');
 });
 
 // Build
-gulp.task('build', ['clean'], function(){
-  gulp.start(['build:css', 'img', 'js', 'fonts', 'html']);
+gulp.task('build', ['clean'], () => {
+  gulp.start(['build:css', 'img', 'icons', 'libs', 'js', 'fonts', 'html']);
 });
 
+const correctNumber = number => number < 10 ? '0' + number : number;
+
+const getDateTime = () => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = correctNumber(now.getMonth() + 1);
+  const day = correctNumber(now.getDate());
+  const hours = correctNumber(now.getHours());
+  const minutes = correctNumber(now.getMinutes());
+
+  return `${year}-${month}-${day}-${hours}${minutes}`;
+};
+
 // Pack dist folder to `pkg.name`.zip
-gulp.task('zip', function () {
+gulp.task('zip', () => {
+  const datetime = getDateTime();
+  const zipName = pkg.name + '-' + datetime + '.zip';
+
   return gulp.src(['./dist/**/*'])
-    .pipe($.zip(pkg.name + '.zip'))
+    .pipe($.zip(zipName))
     .pipe(gulp.dest('.'));
 });
 
 // Watch task
-gulp.task('watch', ['css', 'img', 'js', 'fonts', 'html'], function() {
+gulp.task('watch', () => {
 
-  $.watch('./src/css/**/*.css', function(){
-    gulp.start('css')
-  });
+  $.watch('./src/css/**/*.css', () => { gulp.start('css') });
 
-  $.watch('./src/js/**/*.js', function(){
-    gulp.start('js')
-  });
+  $.watch('./src/js/**/*.js', () => { gulp.start('js') });
 
-  $.watch('./src/*.html', function(){
-    gulp.start('html');
-  });
+  $.watch('./src/*.html', () => { gulp.start('html') });
 
-  $.watch(paths.fonts, function(){
-    gulp.start('fonts')
-  });
+  $.watch(paths.fonts, () => { gulp.start('fonts') });
 
-  $.watch(paths.img, function(){
-    gulp.start('img')
-  });
+  $.watch(paths.img, () => { gulp.start('img') });
+
+  $.watch(paths.icons, () => { gulp.start('icons') });
 
   browserSync.init({
     files:   ['./dist/**/*', '!dist/**/*.map'],
@@ -123,7 +177,7 @@ gulp.task('watch', ['css', 'img', 'js', 'fonts', 'html'], function() {
     notify:  false,
     ui:      false,
     server:  {
-      baseDir: "./dist"
+      baseDir: './dist'
     }
   });
 
